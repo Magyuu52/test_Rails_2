5611b775a53439347ab9ba7009c8b4fc329fe9ad
Magyuu52 <sota.n509852@gmail.com>
2023-10-18 20:42:27 +0900

    新規登録後のアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confim))
+    if @user.save
+      redirect_to("/")
+    else
+      render "new"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
