b3bfceda3ddd814c4b5303cae7e50dde571c78c9
Magyuu52 <sota.n509852@gmail.com>
2023-10-18 11:31:10 +0900

    Userクラスのresoucesのコントローラーとそのルーティングを用意（動作確認で初めにnewリンクを作成）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'reservations/index'
   get 'rooms/index'
   get '/' => "home#top"
-  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
