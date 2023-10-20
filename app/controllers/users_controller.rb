5c594c0ccb365086d0175d1ff30ecf3966a145b4
Magyuu52 <sota.n509852@gmail.com>
2023-10-20 12:35:38 +0900

    間違えて作成したカラムを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: [:email],
-      password: [password],
-      password_confim: [password_confim],
+      
       image_name: 'default-avatar-7a6cbfd7993e89f24bfc888f4a035a83c6f1428b8bdc47eed9095f2799a40153.png')
     if @user.save
       session[:user_id] = @user.id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_20_022445) do
+ActiveRecord::Schema.define(version: 2023_10_20_033156) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2023_10_20_022445) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
-    t.string "image"
   end
 
 end
