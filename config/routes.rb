3c493566f0b9c35901b17e23add4cc766271f9c9
Magyuu52 <sota.n509852@gmail.com>
2023-10-20 12:17:55 +0900

    ログイン機能を追加するために、urlのデザインとルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, :only => [:new, :create, :edit, :update]
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
   get 'users/account' => 'users#account'
   get 'users/profile' => 'users#profile'
-  get 'login' => 'users#login_form'
 end
