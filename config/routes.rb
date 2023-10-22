c6d40530db536faa2858ee8d708e54c0a4025340
Magyuu52 <sota.n509852@gmail.com>
2023-10-22 11:05:48 +0900

    ユーザークラスに自己紹介のためのカラムを追加し、編集画面を用意

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
 
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, :only => [:new, :create, :edit, :update]
+  resources :users, :only => [:new, :create, :update]
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   get 'users/account' => 'users#account'
+  get 'users/account/edit' => 'users#edit'
   get 'users/profile' => 'users#profile'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_20_033156) do
+ActiveRecord::Schema.define(version: 2023_10_22_020014) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_10_20_033156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
+    t.text "introduction"
   end
 
 end
