ad6a01fe8a882862ba2997e724fd39444480ff48
Magyuu52 <sota.n509852@gmail.com>
2023-10-26 07:14:16 +0900

    Roomモデルに指定のバリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ApplicationRecord
+    validates :name, presence: true
+    validates :detail, presence: true
+    validates :price, presence: true, numericality: {greater_than_or_equal_to: 1}
+    validates :adress, presence: true
     has_one_attached :image_room
 end
