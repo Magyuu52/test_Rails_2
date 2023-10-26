9e3ad4ce09b673cee6d20c9d7c1feb56dfa3154b
Magyuu52 <sota.n509852@gmail.com>
2023-10-27 07:54:55 +0900

    userモデルとroomモデルをアソシエーションするための記述を追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,7 @@ class Room < ApplicationRecord
     validates :detail, presence: true
     validates :price, presence: true, numericality: {greater_than_or_equal_to: 1}
     validates :adress, presence: true
+    belongs_to :user
+    
     has_one_attached :image_room
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     validates :password_confim, presence: true
+    has_many :rooms
+
     has_one_attached :image_name
     
 end
