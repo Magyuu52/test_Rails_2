f8d2ccc7a97d660d3de71ffe94d43b28f0202f8d
Magyuu52 <sota.n509852@gmail.com>
2023-11-02 19:17:12 +0900

    Reservationモデルに必要なバリデーションを記載

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :population, presence: true, numericality: {greater_than_or_equal_to: 1}
+  validate :stay_days_restriction
+  validate :check_in_restriction
+
+  def stay_days_restriction
+      errors.add(:チェックアウト, "はチェックインより遅い日付を選択してください") if check_in > check_out
+  end
+    
+  def check_in_restriction
+      errors.add(:チェックイン, "は現在の日時より遅い日付を選択してください") if check_in < Time.now
+  end
 end
