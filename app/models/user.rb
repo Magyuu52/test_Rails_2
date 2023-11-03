9601f85503faa8e64bdd124da7527b65986a0083
Magyuu52 <sota.n509852@gmail.com>
2023-11-04 04:02:37 +0900

    ユーザーのパスワードを『*』で表示されるようにフォームを変更し、パスワードと確認用パスワードを一致させる必要があるバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     validates :password_confim, presence: true
+    validate :check_password
     has_many :rooms
     has_many :reservations
 
     has_one_attached :image_name
     
+    def check_password
+        errors.add(:確認用パスワード,"が一致しません。正しく入力してください") if password_confim != password
+    end
 end
